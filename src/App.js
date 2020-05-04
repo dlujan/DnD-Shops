e77ccf3b39bb7786ee0455383b36b1566770ebce
Daniel Lujan <daniel.lujan96@gmail.com>
2020-05-03 21:07:01 -0500

    began complete redo of redux and signup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import {HashRouter, Switch, Route} from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import jwtDecode from 'jwt-decode';
-import AuthRoute from './util/AuthRoute';
 
 // Redux
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import { SET_AUTHENTICATED } from './redux/types';
-import { logoutUser, getUserData } from './redux/actions/userActions';
+import store from './store';
 
 // Components
 import General from './components/shops/General';
@@ -29,18 +26,18 @@ import Custom from './components/Custom';
 import './App.css';
 import axios from 'axios';
 
-const token = localStorage.FBIdToken;
-if(token) {
-  const decodedToken = jwtDecode(token);
-  if(decodedToken.exp * 1000 < Date.now()){
-    store.dispatch(logoutUser())
-    // window.location.href = '/login';
-  } else {
-    store.dispatch({ type: SET_AUTHENTICATED });
-    axios.defaults.headers.common['Authorization'] = token;
-    store.dispatch(getUserData());
-  }
-}
+// const token = localStorage.FBIdToken;
+// if(token) {
+//   const decodedToken = jwtDecode(token);
+//   if(decodedToken.exp * 1000 < Date.now()){
+//     store.dispatch(logoutUser())
+//     // window.location.href = '/login';
+//   } else {
+//     store.dispatch({ type: SET_AUTHENTICATED });
+//     axios.defaults.headers.common['Authorization'] = token;
+//     store.dispatch(getUserData());
+//   }
+// }
 
 class App extends React.Component {
   constructor(props) {
@@ -74,55 +71,55 @@ class App extends React.Component {
     } else {
         return (
           <Provider store={store}>
-          <HashRouter>
-                <div>
-                  <Navbar/>
-                </div>
-                <div>
-                <Switch>
-                  <Route exact path="/login" render={(props) => <Login {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/signup" render={(props) => <Signup {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/" render={(props) => <General {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/blacksmith" render={(props) => <Blacksmith {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/leatherworker" render={(props) => <Leatherworker {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/fletcher" render={(props) => <Fletcher {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/tailor" render={(props) => <Tailor {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/tavern" render={(props) => <Tavern {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/arcane" render={(props) => <Arcane {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/potions" render={(props) => <Potions {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/temple" render={(props) => <Temple {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/jeweler" render={(props) => <Jeweler {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/animals" render={(props) => <Animals {...props}shops={this.state.shops}/>} />
-                </Switch>
-                <Switch>
-                  <Route exact path="/custom" render={(props) => <Custom {...props}shops={this.state.shops}/>} />
-                </Switch>
-                </div>
-            </HashRouter>
+            <HashRouter>
+                  <div>
+                    <Navbar/>
+                  </div>
+                  <div>
+                  <Switch>
+                    <Route exact path="/login" render={(props) => <Login {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/signup" render={(props) => <Signup {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/" render={(props) => <General {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/blacksmith" render={(props) => <Blacksmith {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/leatherworker" render={(props) => <Leatherworker {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/fletcher" render={(props) => <Fletcher {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/tailor" render={(props) => <Tailor {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/tavern" render={(props) => <Tavern {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/arcane" render={(props) => <Arcane {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/potions" render={(props) => <Potions {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/temple" render={(props) => <Temple {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/jeweler" render={(props) => <Jeweler {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/animals" render={(props) => <Animals {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  <Switch>
+                    <Route exact path="/custom" render={(props) => <Custom {...props}shops={this.state.shops}/>} />
+                  </Switch>
+                  </div>
+              </HashRouter>
             </Provider>
         );
       }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,179 +1,100 @@
-import React, { Component } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
-import withStyles from '@material-ui/core/styles/withStyles';
-import PropTypes from 'prop-types';
-
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import axios from 'axios';
 
 import { connect } from 'react-redux';
-import { signupUser } from '../redux/actions/userActions';
 
-const styles = {
-    form: {
-        textAlign: 'center'
-    },
-    gridMid: {
-        margin: '0 2.5rem'
-    },
-    pageTitle: {
-        margin: '10px auto 10px auto'
-    },
-    textField: {
-        margin: '10px auto 10px auto'
-    },
-    button: {
-        margin: '18px 0',
-        position: 'relative'
-    },
-    customError: {
-        color: 'red',
-        fontSize: '0.8rem',
-        marginTop: 10
-    },
-    progress: {
-        position: 'absolute'
-    }
-};
-class Signup extends Component {
-    constructor() {
-        super();
-        this.state = {
-            email: '',
-            password: '',
-            confirmPassword: '',
-            handle: '',
-            errors: {}
-        }
-    }
+const Signup = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.UI.errors) {
-            this.setState({ errors: nextProps.UI.errors });
-        }
-    }
+    const { name, email, password, password2 } = formData;
 
-    handleSubmit = (event) => {
+    const onChange = event => setFormData({ ...formData, [event.target.name]: event.target.value});
+
+    const onSubmit = async event => {
         event.preventDefault();
-        this.setState({
-            loading: true
-        });
-        const newUserData = {
-            email: this.state.email,
-            password: this.state.password,
-            confirmPassword: this.state.confirmPassword,
-            handle: this.state.handle
+        if (password !== password2) {
+            console.log('passwords do not match')
+        } else {
+            const newUser = {
+                name,
+                email,
+                password
+            }
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                const body = JSON.stringify(newUser);
+
+                const res = await axios.post();
+            } catch(err) {
+
+            }
         }
-        this.props.signupUser(newUserData, this.props.history);
-
-    }
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
     }
 
-    render() {
-        const {classes, UI: { loading }} = this.props;
-        const{errors} = this.state;
-
-        return (
-            <Grid container className={classes.form}>
-                <Grid item sm/>
-                <Grid item sm className={classes.gridMid}>
-                    <p>img tag app icon goes here</p>
-                    <Typography variant='h3' className={classes.pageTitle}>
-                        Signup
-                    </Typography>
-                    <form noValidate onSubmit={this.handleSubmit}>
-                        <TextField 
-                            id="email"
-                            name="email"
-                            type="email"
-                            label="Email"
-                            className={classes.textField}
-                            helperText={errors.email}
-                            error={errors.email ? true : false}
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                            fullWidth
-                        />
-                        <TextField 
-                            id="password"
-                            name="password"
-                            type="password"
-                            label="Password"
-                            className={classes.textField} 
-                            helperText={errors.password}
-                            error={errors.password ? true : false}
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            fullWidth
-                        />
-                        <TextField 
-                            id="confirmPassword"
-                            name="confirmPassword"
-                            type="password"
-                            label="Confirm Password"
-                            className={classes.textField} 
-                            helperText={errors.confirmPassword}
-                            error={errors.confirmPassword ? true : false}
-                            value={this.state.confirmPassword}
-                            onChange={this.handleChange}
-                            fullWidth
-                        />
-                        <TextField 
-                            id="handle"
-                            name="handle"
-                            type="text"
-                            label="Username"
-                            className={classes.textField} 
-                            helperText={errors.handle}
-                            error={errors.handle ? true : false}
-                            value={this.state.handle}
-                            onChange={this.handleChange}
-                            fullWidth
-                        />
-                        {errors.general && (
-                            <Typography variant="body2" className={classes.customError}>
-                                {errors.general}
-                            </Typography>
-                        )}
-                        <Button 
-                            type="submit"
-                            variant="contained"
-                            color="primary"
-                            className={classes.button}
-                            disabled={loading}
-                        >
-                            Signup
-                            {loading && (
-                                <CircularProgress size={30} className={classes.progress}/>
-                            )}
-                        </Button>
-                        <br/>
-                        <small>Already have an account? Login <Link to="/login">here</Link></small>
-                    </form>
-                </Grid>
-                <Grid item sm/>
-            </Grid>
-        )
-    }
+    return (
+        <Fragment>
+            <h1>Sign Up</h1>
+            <p><i></i> Create Your Account</p>
+            <form onSubmit={event => onSubmit(event)}>
+                <div>
+                    <input 
+                        type="text" 
+                        placeholder="Name" 
+                        name="name" 
+                        value={name} 
+                        onChange={event => onChange(event)}
+                        required 
+                    />
+                </div>
+                <div>
+                    <input 
+                        type="email" 
+                        placeholder="Email Address" 
+                        name="email"
+                        value={email}
+                        onChange={event => onChange(event)}
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        value={password}
+                        onChange={event => onChange(event)}
+                        required
+                    />
+                </div>
+                <div>
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        minLength="6"
+                        value={password2}
+                        onChange={event => onChange(event)}
+                        required
+                    />
+                </div>
+                <input type="submit" value="Submit" />
+            </form>
+            <p>
+                Already have an account? <Link to="/login">Login</Link>
+            </p>
+        </Fragment>
+    )
 }
 
-Signup.propTypes = {
-    classes: PropTypes.object.isRequired,
-    user: PropTypes.object.isRequired,
-    UI: PropTypes.object.isRequired,
-    signupUser: PropTypes.func.isRequired
-};
-
-const mapStateToProps = (state) => ({
-    user: state.user,
-    UI: state.UI
-})
-
-export default connect(mapStateToProps, { signupUser })(withStyles(styles)(Signup));
+export default Signup;
